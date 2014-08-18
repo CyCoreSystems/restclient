c945634a9b5d4a0e0a3b926f1cc0e9126f4d6fdc
William Edward Lee <theelee13@gmail.com>
2014-08-18 19:10:07 -0400

    added simple testing

---
+++ restclient_test.go
@@ -0,0 +1,41 @@
+package restclient
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var auth = new(Auth)
+
+func init() {
+	auth.Username = "edward"
+	auth.Password = "pass"
+}
+
+func TestNewRequest(t *testing.T) {
+	assert := assert.New(t)
+	req := NewRequest("GET", "url.com", *auth)
+	AuthTester(t, *auth, req.Auth)
+	assert.Equal(req.Method, "GET", "Method should match call")
+	assert.Equal(req.Url, "url.com", "URL should match call")
+}
+
+//this method exists solely to clean up the amount of auth tests.
+//this is simply a guarantee that no authentication is ever mangled.
+func AuthTester(t *testing.T, auth Auth, reqAuth Auth) {
+	assert := assert.New(t)
+	assert.Equal(auth.Username, reqAuth.Username, "Usernames should be equal")
+	assert.Equal(auth.Password, reqAuth.Password, "Passwords should be equal")
+}
+
+func TestCreateRequest(t *testing.T) {
+	assert := assert.New(t)
+	req := NewRequest("GET", "url.com", *auth)
+	err := req.createHTTPRequest()
+	assert.Nil(err)
+	assert.Equal(req.Request.Method, "GET", "Method should match call")
+	assert.Equal(req.Request.URL.Path, "url.com", "URL should match call")
+	assert.Equal(req.Request.Proto, "HTTP/1.1")
+	assert.NotNil(req.Request.Header)
+}
