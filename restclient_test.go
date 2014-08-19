327eb9ff08dbb07c17fa5b69b703d7ba1fe334df
William Edward Lee <theelee13@gmail.com>
2014-08-18 20:56:49 -0400

    will finish working on response at some point

--- restclient_test.go
+++ restclient_test.go
@@ -1,6 +1,7 @@
 package restclient
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,13 +22,16 @@ func TestNewRequest(t *testing.T) {
 	assert.Equal(req.Url, "url.com", "URL should match call")
 }
 
-//this method exists solely to clean up the amount of auth tests.
 //this is simply a guarantee that no authentication is ever mangled.
 func AuthTester(t *testing.T, auth Auth, reqAuth Auth) {
 	assert := assert.New(t)
 	assert.Equal(auth.Username, reqAuth.Username, "Usernames should be equal")
 	assert.Equal(auth.Password, reqAuth.Password, "Passwords should be equal")
 }
+
+/*
+	Some simple tests to guarantee no data is somehow mangled on transport
+*/
 
 func TestCreateRequest(t *testing.T) {
 	assert := assert.New(t)
@@ -48,15 +52,53 @@ func TestCreateClient(t *testing.T) {
 	assert.NotNil(req.Client.Transport)
 }
 
-type TestStruct struct {
+type TestStructRequest struct {
 	Variable string `json:"variable"`
 }
+
+/*
+	Create an arbitrary json-ified struct and pass it through.
+	The encoder should not register any errors and should return
+	a map (non-nil), on success.
+*/
 
 func TestEncodeRequest(t *testing.T) {
 	assert := assert.New(t)
 	req := NewRequest("GET", "url.com", *auth)
-	req.RequestBody = TestStruct{"hi"}
+	req.RequestBody = TestStructRequest{"hi"}
 	err := req.EncodeRequestBody()
 	assert.Nil(err)
 	assert.NotNil(req.RequestReader)
+}
+
+/*
+	This method just tries a few different possible status codes.
+	All we're testing for now is that none of the errors on response
+	are nil, unless we pass through a 200.
+*/
+func TestProcessStatusCode(t *testing.T) {
+	assert := assert.New(t)
+	req := NewRequest("GET", "url.com", *auth)
+	req.Response = new(http.Response)
+	req.Response.StatusCode = 404
+	err := req.ProcessStatusCode()
+	assert.NotNil(err)
+	req.Response.StatusCode = 450
+	err = req.ProcessStatusCode()
+	assert.NotNil(err)
+	req.Response.StatusCode = 550
+	err = req.ProcessStatusCode()
+	assert.NotNil(err)
+	req.Response.StatusCode = 200
+	err = req.ProcessStatusCode()
+	assert.Nil(err)
+}
+
+/*
+ */
+
+func TestDecodeResponse(t *testing.T) {
+	assert := assert.New(t)
+	req := NewRequest("GET", "url.com", *auth)
+	req.Response = new(http.Response)
 }
